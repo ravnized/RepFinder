f915ded636038dbfdf4d086da7a8db1e8e01dbf4
Gianpaolo Torino <43211190+ravnized@users.noreply.github.com>
2022-11-06 22:14:15 +0000

    update

--- backend/src/scraper.ts
+++ backend/src/scraper.ts
@@ -6,32 +6,24 @@ export default class ScraperDao {
 
     static async scraperTest() {
         try {
-
-            
-
-
-
             fetch('https://o832club.x.yupoo.com/', {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
                 },
             }).then((response) => {
+                console.log("got response")
                 console.log(response)
-                //const $ = cheerio.load(response)
-                /*
-                 $('.showindex__children').each((index: number, element: any) => {
-                     const elementButton = $(element).find('a');
-                     const title = elementButton.attr('title');
-                     var image = elementButton.find('.album__img');
-                     //elementButton.find('.album__img').attr('data-origin-src');
-                     if (image === undefined) {
- 
-                     }
-                     //console.log(`index ${index}, Title:${title} `)
-                     console.log(`image src: ${image}`)
-                 });
-                 */
+                const $ = cheerio.load(response);
+                $('.showindex__children').each((index: number, element: any) => {
+                    const elementButton = $(element).find('a');
+                    const title = elementButton.attr('title');
+                    var image = elementButton.find('.album__img').text();
+                    //elementButton.find('.album__img').attr('data-origin-src');
+                    //console.log(`index ${index}, Title:${title} `)
+                    console.log(`image src: ${image}`)
+                });
+
 
             })
                 .catch((error) => {
